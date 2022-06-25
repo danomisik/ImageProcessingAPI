53a64b59eeb8d52aa8955d1e158ab537b13717b0
danomisik <misik.fiit@gmail.com>
2022-06-25 10:17:51 +0200

    Add types for image route, exception throwing for image route, clean dependencies

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,21 +1,26 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import imageResize from '../utils/imageResize';
 
 const routes = express.Router();
 
-routes.get('/images', async (req, res) => {
+routes.get('/images', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   //console.log(`filename: ${req.query.filename}`);
   //console.log(`weight: ${req.query.weight}`);
   //console.log(`height: ${req.query.height}`);
-  const result = await imageResize(
-    String(req.query.filename),
-    Number(req.query.weight),
-    Number(req.query.height)
-  );
-  if (result) {
-    res.sendFile(`src/assets/thumb/${req.query.filename}.jpg`, { root: '.' });
-  } else {
-    res.send("Image resize didn't finish with success.");
+  try {
+    const result = await imageResize(
+      String(req.query.filename),
+      Number(req.query.weight),
+      Number(req.query.height)
+    );
+    if (result) {
+      res.sendFile(`src/assets/thumb/${req.query.filename}.jpg`, { root: '.' });
+    } else {
+      next("Invalid input parameters");
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
--- src/utils/imageResize.ts
+++ src/utils/imageResize.ts
@@ -1,19 +1,23 @@
 import sharp from "sharp";
+import { existsSync } from 'node:fs';
 
 const imageResize = async (
   filename: string,
   weight: number,
   height: number
 ): Promise<boolean> => {
-  try {
-    await sharp(`src/assets/full/${filename}.jpg`)
-      .resize(weight, height)
-      .toFile(`src/assets/thumb/${filename}.jpg`);
-    return true;
-  } catch (error) {
-    console.log(`Error in sharp transformation: ${error}`);
+  if (!existsSync(`src/assets/full/${filename}.jpg`)) {
+    console.log("File does not exist.");
     return false;
   }
+  if (isNaN(weight) || isNaN(height)) {
+    console.log("Weight and height isn't number.");
+    return false;
+  }
+  await sharp(`src/assets/full/${filename}.jpg`)
+      .resize(weight, height)
+      .toFile(`src/assets/thumb/${filename}.jpg`);
+  return true;
 };
 
 export default imageResize;
